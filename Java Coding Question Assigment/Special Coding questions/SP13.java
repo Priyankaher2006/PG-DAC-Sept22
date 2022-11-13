fa2b12d70691c1edacd67510f4c5d9423848e964
Priyankaher2006 <113438323+Priyankaher2006@users.noreply.github.com>
2022-11-13 17:51:44 +0530

    Add files via upload

---
+++ Java Coding Question Assigment/Special Coding questions/SP13.java
@@ -0,0 +1,50 @@
+/*
+13. Given an encoded string, s, return its decoded representation. The string s will be 
+encoded as follows N[letters], meaning that the letters should be repeated N times in the 
+decoded representation.
+Note: You may assume s always encoded correctly (i.e. correct formatting of brackets, 
+only positive values outside the brackets, and always lowercase alphabetical characters 
+inside the brackets).
+Ex: Given the following string s…
+s = "3[a]2[b]1[c]", return "aaabbc" ("a" is repeated 3 times, "b" is repeated 2 times, and 
+"c" is repeated 1 time).
+*/
+
+import java.util.*;
+class SP13 {
+    
+    int i = 0;
+
+    public String decodeString(String s) {
+        int count = 0;
+        StringBuilder sb = new StringBuilder();
+        while (i < s.length()) {
+            char c = s.charAt(i);
+            i++;
+            if (Character.isLetter(c)) {
+                sb.append(c);
+            } else if (Character.isDigit(c)) {
+                count = count * 10 + Character.getNumericValue(c);
+            } else if (c == ']') {
+                break;
+            } else if (c == '[') {
+                // sub problem
+                String repeat = decodeString(s);
+                while (count > 0) {
+                    sb.append(repeat);
+                    count--;
+                }
+            }
+        }
+        return sb.toString();
+    }
+
+	public static void main(String []args)
+	{
+
+		String str="3[a]2[b]1[c]";
+		SP13 d=new SP13();
+		System.out.println(d.decodeString(str));
+	}
+}
+/*aaabbc*/
